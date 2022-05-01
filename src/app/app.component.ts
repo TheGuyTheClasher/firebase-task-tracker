8e29d526dc672570a2d2161ebca305cbf1c312d2
TheGuyTheClasher <nishikant.bawache.nb@gmail.com>
2022-05-01 18:04:56 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,104 @@
 import { Component } from '@angular/core';
+import { CdkDragDrop, transferArrayItem } from '@angular/cdk/drag-drop';
+import { MatDialog } from '@angular/material/dialog';
+import { Task } from './task/task';
+import { TaskDialogComponent } from './task-dialog/task-dialog.component';
+import { TaskDialogResult } from './task-dialog/task-dialog.component';
+
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'Task-tracker';
+  // todo: Task[] = [
+  //   {
+  //     title: 'Buy milk',
+  //     description: 'Go to the store and buy milk',
+  //   },
+  //   {
+  //     title: 'Create a Kanban app',
+  //     description: 'Using Firebase and Angular create a Kanban app!',
+  //   },
+  // ];
+  // inProgress: Task[] = [];
+  // done: Task[] = [];
+
+  todo = this.store
+    .collection('todo')
+    .valueChanges({ idField: 'id' }) as Observable<Task[]>;
+  inProgress = this.store
+    .collection('inProgress')
+    .valueChanges({ idField: 'id' }) as Observable<Task[]>;
+  done = this.store
+    .collection('done')
+    .valueChanges({ idField: 'id' }) as Observable<Task[]>;
+
+  constructor(private dialog: MatDialog, private store: AngularFirestore) {}
+
+  newTask(): void {
+    const dialogRef = this.dialog.open(TaskDialogComponent, {
+      width: '270px',
+      data: {
+        task: {},
+      },
+    });
+    dialogRef
+      .afterClosed()
+      .subscribe((result: TaskDialogResult | undefined) => {
+        if (!result) {
+          return;
+        }
+        // this.todo.push(result.task);
+        this.store.collection('todo').add(result.task);
+      });
+  }
+
+  editTask(list: 'done' | 'todo' | 'inProgress', task: Task): void {
+    const dialogRef = this.dialog.open(TaskDialogComponent, {
+      width: '270px',
+      data: {
+        task,
+        enableDelete: true,
+      },
+    });
+    dialogRef
+      .afterClosed()
+      .subscribe((result: TaskDialogResult | undefined) => {
+        if (!result) {
+          return;
+        }
+        // const dataList = this[list];
+        // const taskIndex = dataList.indexOf(task);
+        if (result.delete) {
+          this.store.collection(list).doc(task.id).delete();
+        } else {
+          this.store.collection(list).doc(task.id).update(task);
+        }
+      });
+  }
+
+  drop(event: CdkDragDrop<Task[] | null>): void {
+    if (event.previousContainer === event.container) {
+      return;
+    }
+    const item = event.previousContainer.data[event.previousIndex];
+    this.store.firestore.runTransaction(() => {
+      const promise = Promise.all([
+        this.store.collection(event.previousContainer.id).doc(item.id).delete(),
+        this.store.collection(event.container.id).add(item),
+      ]);
+      return promise;
+    });
+    transferArrayItem(
+      event.previousContainer.data,
+      event.container.data,
+      event.previousIndex,
+      event.currentIndex
+    );
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,40 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
+
+import { environment } from 'src/environments/environment';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { TaskComponent } from './task/task.component';
+import { TaskDialogComponent } from './task-dialog/task-dialog.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, TaskComponent, TaskDialogComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NoopAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatCardModule,
+    DragDropModule,
+    MatButtonModule,
+    MatDialogModule,
+    MatInputModule,
+    FormsModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFirestoreModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
